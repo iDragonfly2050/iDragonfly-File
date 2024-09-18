7c51b17a5ddb18c36ba2376bb59c9f31e68bb547
iDragonfly <1099965906@qq.com>
2024-09-18 15:44:33 +0800

    Update PDD.user.js

--- HYC/PDD.user.js
+++ HYC/PDD.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Remove PDD Ads
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  自动移除拼多多广告
 // @author       You
 // @include      https://*.pinduoduo.com/*
