d823982e95ac94a18d75a7d550f0c1381fee2fc9
iDragonfly <1099965906@qq.com>
2024-09-18 15:32:03 +0800

    Update PDD.user.js

--- HYC/PDD.user.js
+++ HYC/PDD.user.js
@@ -13,7 +13,7 @@
 (function() {
     'use strict';
 
-    // 定义一个函数来移除广告2222222222222222222111
+    // 定义一个函数来移除广告3333333333333333333
     function removeAds() {
         // 根据广告的 CSS 类、ID 或标签名进行选择并移除
         let adElements = document.querySelectorAll('.ad, .advertisement, .ads, [id*="ad"]');
